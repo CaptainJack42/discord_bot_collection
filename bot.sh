6098f42d7fad57196ff84b8b8f105408d2461e1f
Janek <janek.graff@gmx.de>
2022-02-04 11:53:58 +0100

    add example start script

---
+++ bot.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+### BEGIN INIT INFO
+# Provides:          fancontrol.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
+
+# Carry out specific functions when asked to by the system
+
+case "$1" in
+    start)
+        echo "Starting Discord Bot"
+        /home/pi/discord_bot_collection/bot.py &
+        ;;
+    stop)
+        echo "Stopping Discord Bot"
+        pkill -f /home/pi/discord_bot_collection/bot.py
+        ;;
+    *)
+        echo "Usage: ./bot.sh {start|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0
